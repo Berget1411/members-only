8ddb174d0eafa3818b00c5ac9e672294da00c360
Berget1411 <ludvig.benjaminbergstrom@gmail.com>
2024-03-03 09:35:36 +0100

    setup mongodb database

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const indexRouter = require('./routes/index');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
-app.set('view engine', 'ejs');
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(3000))
+  .catch((err) => console.log(err));
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(indexRouter);
-
-app.listen(3000);
